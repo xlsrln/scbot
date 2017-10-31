a832985241df5ad4bb2abf18a80580239a4f3108
axel sarlin <ax.flumm@gmail.com>
2017-10-31 16:11:53 +0100

    borde returna saker på employergrejen

--- src/Employer.java
+++ src/Employer.java
@@ -1,18 +1,22 @@
 import bwapi.*;
-import bwta.BWTA;
-import bwta.BaseLocation;
 
 public class Employer {
 	Unit unit = null;
 	
 	public Unit findWorker() {
+		Unit unit = null;
 		for(Unit u : self.getUnits())
 		{
 			if(u.getType().isWorker())
 			{
-				return u;
+				unit = u;
 			}
 		}
+<<<<<<< Updated upstream
+=======
+		return unit;
+		
+>>>>>>> Stashed changes
 	}
 	
 	//methods to do stuff with the worker, i.e. build a building
